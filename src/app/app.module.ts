0e7b076bfcc6de1ec5e778a650fbdd348d93fd3f
Nikola <nikolaguzina.ns@gmail.com>
2018-03-07 20:06:40 +0100

    Imported modules and openweather.serrvice.ts in app.module.ts .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WeatherComponent } from './weather/weather.component';
 import { WeatherCityComponent } from './weather/weather-city/weather-city.component';
-
+import { OpenweatherService } from './services/openweather.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,13 @@ import { WeatherCityComponent } from './weather/weather-city/weather-city.compon
     WeatherCityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    OpenweatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
