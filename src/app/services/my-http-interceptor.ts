e2131d1f4906bee9ee4578e5e9ee802ae72b0c2b
Nikola <nikolaguzina.ns@gmail.com>
2018-03-07 22:03:37 +0100

    Deleting erroMsg and empty spaces.

--- src/app/services/my-http-interceptor.ts
+++ src/app/services/my-http-interceptor.ts
@@ -5,17 +5,14 @@ import 'rxjs/add/operator/catch';
 import { Weather } from '../models/weather.model';
 
 export class MyHttpInterceptor implements HttpInterceptor {
-	public errorMsg: string;
 	
 	intercept(req: HttpRequest<Weather> , next: HttpHandler) {
 	
-
 		return next.handle(req).catch((error) => {
 			if (error instanceof HttpErrorResponse) {
 				console.log('This is error:' + error);
 				if (error.status === 404) {
 					console.log('City doesnt exist!!!');
-					this.errorMsg = 'City doesnt exist!!!'
 				} 
 			}
 		});
